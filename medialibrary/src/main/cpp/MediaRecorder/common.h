762b7a1dd3ebaf03d80acf7789c58c19fb1ab1f5
daimaren <975808394@qq.com>
2019-10-09 23:27:03 +0800

    优化MediaRecorder

--- medialibrary/src/main/cpp/MediaRecorder/common.h
+++ medialibrary/src/main/cpp/MediaRecorder/common.h
@@ -29,4 +29,10 @@
 
 typedef unsigned char byte;
 
+static inline long getCurrentTime()
+{
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
 #endif //CAINCAMERA_COMMON_H
