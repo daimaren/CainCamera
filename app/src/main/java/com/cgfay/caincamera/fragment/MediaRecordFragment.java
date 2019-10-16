8da839038c496a8da246b456750d82be6d81ca88
daimaren <975808394@qq.com>
2019-10-16 23:32:23 +0800

    Revert: rename

--- app/src/main/java/com/cgfay/caincamera/fragment/MediaRecordFragment.java
+++ app/src/main/java/com/cgfay/caincamera/fragment/MediaRecordFragment.java
@@ -1,11 +1,9 @@
 package com.cgfay.caincamera.fragment;
 
-import android.Manifest;
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.media.AudioManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,47 +12,44 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.cgfay.caincamera.R;
-import com.cgfay.caincamera.presenter.FFMediaRecordPresenter;
-import com.cgfay.caincamera.presenter.MediaRecordPresenter;
-import com.cgfay.caincamera.renderer.FFRecordRenderer;
-import com.cgfay.caincamera.renderer.MediaRecordRenderer;
 import com.cgfay.caincamera.widget.GLRecordView;
+import com.cgfay.camera.engine.model.CameraInfo;
+import com.cgfay.camera.utils.PathConstraints;
 import com.cgfay.camera.widget.RecordProgressView;
+import com.cgfay.media.recorder.AVFormatter;
+import com.cgfay.media.recorder.FFMediaRecorder;
+import com.cgfay.media.recorder.MediaRecorder;
 import com.cgfay.uitls.utils.NotchUtils;
-import com.cgfay.uitls.utils.PermissionUtils;
 import com.cgfay.uitls.utils.StatusBarUtils;
 
-/**
- * 利用FFmpeg录制视频
- */
-public class MediaRecordFragment extends Fragment implements View.OnClickListener {
+public class MediaRecordFragment extends Fragment implements View.OnClickListener, SurfaceHolder.Callback,
+        Camera.PreviewCallback, SurfaceTexture.OnFrameAvailableListener, MediaRecorder.OnRecordListener{
 
-    private static final String TAG = "MediaRecordFragment";
+    private static final String TAG = "FFMediaRecordFragment";
+    private static final int CAMERA_FACING_BACK = 0;
+    private static final int CAMERA_FACING_FRONT = 1;
 
     private Activity mActivity;
     private Handler mMainHandler;
 
     private View mContentView;
-    private GLRecordView mGLRecordView;
+    private SurfaceView mSVRecordView;
     private RecordProgressView mProgressView;
     private Button mRecordButton;
 
     private Button mBtnNext;
     private Button mBtnDelete;
 
-    private AudioManager mAudioManager;
-
-    private MediaRecordPresenter mPresenter;
-    private MediaRecordRenderer mRenderer;
-
+    private MediaRecorder mMediaRecorder;
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -71,26 +66,24 @@ public class MediaRecordFragment extends Fragment implements View.OnClickListene
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mContentView = inflater.inflate(R.layout.fragment_ffmedia_record, container, false);
+        mContentView = inflater.inflate(R.layout.fragment_media_record, container, false);
         return mContentView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (mAudioManager == null) {
-            mAudioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
-            mAudioManager.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-        }
-        mPresenter = new MediaRecordPresenter(mActivity, this);
-        mPresenter.setRecordSeconds(15);
-        mRenderer = new MediaRecordRenderer(mPresenter);
+        mMediaRecorder = new MediaRecorder.RecordBuilder(generateOutputPath())
+                .create();
+        mMediaRecorder.setRecordListener(this);
+        initView();
+        bindListener();
+    }
 
+    private void initView() {
         // 录制预览
-        mGLRecordView = (GLRecordView) mContentView.findViewById(R.id.gl_record_view);
-        mGLRecordView.setEGLContextClientVersion(3);
-        mGLRecordView.setRenderer(mRenderer);
-        mGLRecordView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        mSVRecordView = (SurfaceView) mContentView.findViewById(R.id.sv_record_view);
+        mSVRecordView.getHolder().addCallback(this);
         // 进度条
         mProgressView = (RecordProgressView) mContentView.findViewById(R.id.record_progress_view);
 
@@ -98,19 +91,7 @@ public class MediaRecordFragment extends Fragment implements View.OnClickListene
         mRecordButton = (Button) mContentView.findViewById(R.id.record_button);
         mRecordButton.setOnClickListener(v -> {
             mRecordButton.setEnabled(false);
-            if (mPresenter.isRecording()) {
-                mPresenter.stopRecord();
-            } else {
-                mPresenter.startRecord();
-            }
-            mMainHandler.postDelayed(() -> {
-                if (mPresenter.isRecording()) {
-                    mRecordButton.setText(getText(R.string.btn_record_cancel));
-                } else {
-                    mRecordButton.setText(getText(R.string.btn_record_start));
-                }
-                mRecordButton.setEnabled(true);
-            }, 1000);
+            //todo
         });
 
         // 下一步
@@ -130,165 +111,74 @@ public class MediaRecordFragment extends Fragment implements View.OnClickListene
         }
     }
 
+    private void bindListener() {
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        handleFullScreen();
-        mGLRecordView.onResume();
-        mPresenter.onResume();
-        mPresenter.setAudioEnable(PermissionUtils.permissionChecking(this, Manifest.permission.RECORD_AUDIO));
+    public void onClick(View view) {
+
+    }
+
+    // surface callback
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        if (mMediaRecorder != null)
+            mMediaRecorder.prepareEGLContext(surfaceHolder.getSurface(), mSVRecordView.getWidth(), mSVRecordView.getHeight(), CAMERA_FACING_FRONT);
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mGLRecordView.onPause();
-        mPresenter.onPause();
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
     }
 
     @Override
-    public void onDestroy() {
-        mPresenter.release();
-        mPresenter = null;
-        super.onDestroy();
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
     }
 
-    private void runOnUiThread(Runnable runnable) {
-        mMainHandler.post(runnable);
-    }
+    // camera callback
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
 
-    private void handleFullScreen() {
-        mActivity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-        // 是否全面屏
-        if (NotchUtils.hasNotchScreen(mActivity)) {
-            mActivity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            WindowManager.LayoutParams lp = mActivity.getWindow().getAttributes();
-            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-            mActivity.getWindow().setAttributes(lp);
-        }
     }
 
     @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.btn_delete) {
-            mPresenter.deleteLastVideo();
-        } else if (id == R.id.btn_next) {
-            mPresenter.mergeAndEdit();
-        }
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+
+    }
+
+    //record callback
+    @Override
+    public void onRecordStart() {
+
+    }
+
+    @Override
+    public void onRecording(float duration) {
+
+    }
+
+    @Override
+    public void onRecordFinish(boolean success, float duration) {
+
+    }
+
+    @Override
+    public void onRecordError(String msg) {
+
     }
 
     /**
-     * 隐藏控件
+     * 创建合成的视频文件名
+     * @return
      */
-    public void hidViews() {
-        runOnUiThread(() -> {
-            mBtnNext.setVisibility(View.GONE);
-            mBtnDelete.setVisibility(View.GONE);
-        });
-    }
-
-    /**
-     * 显示控件
-     */
-    public void showViews() {
-        runOnUiThread(() -> {
-            if (mPresenter.getRecordVideos() > 0) {
-                mBtnNext.setVisibility(View.VISIBLE);
-                mBtnDelete.setVisibility(View.VISIBLE);
-            }
-        });
-    }
-
-    /**
-     * 设置进度
-     * @param progress
-     */
-    public void setProgress(float progress) {
-        runOnUiThread(() -> {
-            mProgressView.setProgress(progress);
-        });
-    }
-
-    /**
-     * 添加一段进度
-     * @param progress
-     */
-    public void addProgressSegment(float progress) {
-        runOnUiThread(() -> {
-            mProgressView.addProgressSegment(progress);
-        });
-    }
-
-    /**
-     * 删除一段进度
-     */
-    public void deleteProgressSegment() {
-        runOnUiThread(() -> {
-            mProgressView.deleteProgressSegment();
-        });
-    }
-
-    /**
-     * 更新预览纹理大小
-     * @param width
-     * @param height
-     */
-    public void updateTextureSize(int width, int height) {
-        if (mRenderer != null) {
-            mRenderer.setTextureSize(width, height);
-        }
-    }
-
-    /**
-     * 刷新画面
-     */
-    public void onFrameAvailable() {
-        if (mGLRecordView != null) {
-            mGLRecordView.requestRender();
-        }
-    }
-
-    private Dialog mProgressDialog;
-    /**
-     * 显示合成进度
-     */
-    public void showProgressDialog() {
-        runOnUiThread(() -> {
-            mProgressDialog = ProgressDialog.show(mActivity, "正在合成", "请稍后");
-        });
-    }
-
-    /**
-     * 隐藏合成进度
-     */
-    public void hideProgressDialog() {
-        runOnUiThread(() -> {
-            if (mProgressDialog != null) {
-                mProgressDialog.dismiss();
-                mProgressDialog = null;
-            }
-        });
-    }
-
-    private Toast mToast;
-    /**
-     * 显示Toast提示
-     * @param msg
-     */
-    public void showToast(String msg) {
-        runOnUiThread(() -> {
-            if (mToast != null) {
-                mToast.cancel();
-            }
-            mToast = Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT);
-            mToast.show();
-        });
+    public String generateOutputPath() {
+        return PathConstraints.getVideoCachePath(mActivity);
     }
 }
